409c7a5b97ed620e2ce08c8cc5a81a716677cd6e
martinbel <belmartin@outlook.com>
2014-05-27 00:46:17 -0300

    adding parse_j48 function

--- R/parse_j48.R
+++ R/parse_j48.R
@@ -1,39 +1,37 @@
-#' Parse text of j48 output
+#' Parse j48 text output 
 #' 
-#' @description Uses simple regex to extract the most valuable information from 
-#' the j48 classifier output.
+#' @description
+#' Uses regex to convert a j48() output to a list() with most important information
 #' 
 #' @param model
-#' Output of `j48()`
-#' ## Not Run
-#' ## output <- j48(cmd)
-#' ## models <- parse_j48(output)
+#' output of j48(cmd)
+#' 
 #' @export
-parse_j48 <- function(model){
-  result <- list(); res <- list()
-  for(i in seq_along(as.vector(sapply(model, length)))){
-    correct <- grep("Correctly Classified Instances", model[[i]], val=T)
-    ii = 1; ii2 = 2
-    t1 <- read.table(text = gsub(";$", "", gsub(" %|[[:space:]]{2,}", ";", correct[[ii]])), sep = ";")
-    t2 <- read.table(text = gsub(";$", "", gsub(" %|[[:space:]]{2,}", ";", correct[[ii2]])), sep = ";")
+parse_j48 <- function (model){
+    correct <- grep("Correctly Classified Instances", model, val = T)
+    t1 <- read.table(text = gsub(";$", "", 
+                                 gsub(" %|[[:space:]]{2,}", ";", correct[[1]])), sep = ";")
+    t2 <- read.table(text = gsub(";$", "", 
+                                 gsub(" %|[[:space:]]{2,}", ";", correct[[2]])), sep = ";")
     correct <- rbind(t1, t2)[,-4]
     names(correct) <- c("Desc", "Instances Amount", "Percentage")
+    number_leaves <- as.numeric(gsub("[^[:digit:]]*", "", 
+                                     grep("Number of Leaves", model[[i]], value = TRUE)))
+    tree_size <- as.numeric(gsub("[^[:digit:]]*", "", grep("Size of the tree", 
+                                                           model, value = TRUE)))
+    mean_absolute_error <- 
+      as.numeric(gsub("[[A-Za-z]]*","", grep("Mean absolute error", model, value = TRUE)))
+    number_instances <- 
+      as.numeric(gsub("[^[:digit:]]*", "", grep("Total Number of Instances", model, value = TRUE)))
     
-    number_leaves <- as.numeric(
-      gsub("[^[:digit:]]*", "", grep("Number of Leaves", model[[i]], value = TRUE))
-    )
-    tree_size <- as.numeric(
-      gsub("[^[:digit:]]*", "", grep("Size of the tree", model[[i]], value = TRUE))
-    )
-    mean_absolute_error <- as.numeric(
-      gsub("[[A-Za-z]]*", "", grep("Mean absolute error", model[[i]], value = TRUE))
-    )
-    number_instances <- as.numeric(
-      gsub("[^[:digit:]]*", "", grep("Total Number of Instances", model[[i]], value = TRUE))
-    )
-    result <- list(correct=correct, number_leaves=number_leaves, tree_size=tree_size,
-                   mean_absolute_error=mean_absolute_error, number_instances=number_instances)
-    res[[i]] <- result
-  }
-  res
+    options <- gsub("Options: | $", "", grep("Options.*", model))
+    
+    tree_st <- grep("J48 pruned tree", model)
+    tree_end <- grep("Number of Leaves", model); tree_end <- (tree_end - 1)
+    tree <- model[tree_st:tree_end]
+      
+    result <- list(correct = correct, number_leaves = number_leaves, 
+                   tree_size = tree_size, mean_absolute_error = mean_absolute_error, 
+                   number_instances = number_instances, options=options, tree=tree)
+    result
 }
